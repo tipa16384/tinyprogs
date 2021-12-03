b461f71166f9ae5080fe8b775db4e8d07bab774d
Brenda Holloway <brendahol@gmail.com>
2021-12-03 07:39:20 -0500

    cleaned up the puzzle3 code for posterity.

--- adventofcode2021/puzzle3.py
+++ adventofcode2021/puzzle3.py
@@ -1,23 +1,22 @@
 # this is going to be a super cool program
 # that will be able to do a lot of cool stuff
 
-print ("Totally going to win this one")
-print ("I'm going to be super cool")
-
 fn = 'puzzle3.dat'
+
+# number of bits in the binary string
+width = 12
 
 # read in fn as an array of binary numbers
 def read_values(fn):
     with open(fn) as f:
-        for x in f.read().splitlines():
-            yield x
+        return [x.strip() for x in f.readlines()]
 
+# convert a binary string to an integer
 def binary_to_decimal(binary):
     return int(binary, 2)
 
-width = 12
-
-def calc_gamma(sieve):
+# calculate the gamma and epsilon values according to the rules of the puzzle.
+def calc_gamma_epsilon(sieve):
     bitCount = [0 for x in range(width)]
     count = 0
 
@@ -31,10 +30,7 @@ def calc_gamma(sieve):
     epsilon = ''
 
     for i in bitCount:
-        if i == count/2:
-            gamma += '1'
-            epsilon += '0'
-        elif i > count/2:
+        if i >= count/2:
             gamma += '1'
             epsilon += '0'
         else:
@@ -43,39 +39,37 @@ def calc_gamma(sieve):
 
     return (gamma, epsilon)
 
-gamma, epsilon = calc_gamma(read_values(fn))
+# function to return just the gamma part of the solution
+def calc_gamma(sieve):
+    return calc_gamma_epsilon(sieve)[0]
 
-print (gamma, epsilon)
-print (binary_to_decimal(gamma) * binary_to_decimal(epsilon))
+# function to return the epsilon part of the solution
+def calc_epsilon(sieve):
+    return calc_gamma_epsilon(sieve)[1]
 
-sieve1 = [x for x in read_values(fn)]
+# process the sieve according to the guidance of the function func_select
+# return the solution or raise an exception if none found
+# if none was found, you done screwed up
+def process_sieve(sieve, func_select):
+    for pos in range(width):
+        score = func_select(sieve)
+        sieve = [x for x in sieve if x[pos] == score[pos]]
+        if len(sieve) == 1:
+            return sieve[0]
+    raise Exception('No solution found')
 
-for pos in range(width):
-    gamma, epsilon = calc_gamma(sieve1)
-    sieve2 = [x for x in sieve1 if x[pos] == gamma[pos]]
-    sieve1 = sieve2
-    if len(sieve1) == 1:
-        break
+# main function
+if __name__ == '__main__':
+    gamma, epsilon = calc_gamma_epsilon(read_values(fn))
 
-print (sieve1)
-oxygen = binary_to_decimal(sieve1[0])
+    # part 1 solution
+    print (binary_to_decimal(gamma) * binary_to_decimal(epsilon))
 
-sieve1 = [x for x in read_values(fn)]
+    oxygen = binary_to_decimal(process_sieve(read_values(fn), calc_gamma))
+    co2 = binary_to_decimal(process_sieve(read_values(fn), calc_epsilon))
 
-print (sieve1)
-for pos in range(width):
-    gamma, epsilon = calc_gamma(sieve1)
-    sieve2 = [x for x in sieve1 if x[pos] == epsilon[pos]]
-    sieve1 = sieve2
-    print (sieve1)
-    if len(sieve1) == 1:
-        break
-
-print (sieve1)
-co2 = binary_to_decimal(sieve1[0])
-
-print (oxygen, co2)
-print (oxygen * co2)
+    # part 2 solution
+    print (oxygen * co2)
 
 
 
