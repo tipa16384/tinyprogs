5285d97e8cca31f9ae55c12a852f44be5fa4509e
Brenda Holloway <brendahol@gmail.com>
2021-12-03 07:58:00 -0500

    mehr aufgeräumt

--- adventofcode2021/puzzle3.py
+++ adventofcode2021/puzzle3.py
@@ -6,17 +6,20 @@ fn = 'puzzle3.dat'
 # number of bits in the binary string
 width = 12
 
-# read in fn as an array of binary numbers
+
 def read_values(fn):
+    "read in fn as an array of binary numbers"
     with open(fn) as f:
         return [x.strip() for x in f.readlines()]
 
-# convert a binary string to an integer
+
 def binary_to_decimal(binary):
+    "convert a binary string to an integer"
     return int(binary, 2)
 
-# calculate the gamma and epsilon values according to the rules of the puzzle.
+
 def calc_gamma_epsilon(sieve):
+    "calculate the gamma and epsilon values according to the rules of the puzzle."
     bitCount = [0 for x in range(width)]
     count = 0
 
@@ -26,8 +29,7 @@ def calc_gamma_epsilon(sieve):
             if x[i] == '1':
                 bitCount[i] += 1
 
-    gamma = ''
-    epsilon = ''
+    gamma = epsilon = ''
 
     for i in bitCount:
         if i >= count/2:
@@ -39,18 +41,13 @@ def calc_gamma_epsilon(sieve):
 
     return (gamma, epsilon)
 
-# function to return just the gamma part of the solution
-def calc_gamma(sieve):
-    return calc_gamma_epsilon(sieve)[0]
 
-# function to return the epsilon part of the solution
-def calc_epsilon(sieve):
-    return calc_gamma_epsilon(sieve)[1]
-
-# process the sieve according to the guidance of the function func_select
-# return the solution or raise an exception if none found
-# if none was found, you done screwed up
 def process_sieve(sieve, func_select):
+    """
+    process the sieve according to the guidance of the function func_select
+    return the solution or raise an exception if none found
+    if none was found, you done screwed up
+    """
     for pos in range(width):
         score = func_select(sieve)
         sieve = [x for x in sieve if x[pos] == score[pos]]
@@ -58,19 +55,18 @@ def process_sieve(sieve, func_select):
             return sieve[0]
     raise Exception('No solution found')
 
+
 # main function
 if __name__ == '__main__':
     gamma, epsilon = calc_gamma_epsilon(read_values(fn))
 
     # part 1 solution
-    print (binary_to_decimal(gamma) * binary_to_decimal(epsilon))
+    print(binary_to_decimal(gamma) * binary_to_decimal(epsilon))
 
-    oxygen = binary_to_decimal(process_sieve(read_values(fn), calc_gamma))
-    co2 = binary_to_decimal(process_sieve(read_values(fn), calc_epsilon))
+    oxygen = binary_to_decimal(process_sieve(
+        read_values(fn), lambda x: calc_gamma_epsilon(x)[0]))
+    co2 = binary_to_decimal(process_sieve(
+        read_values(fn), lambda x: calc_gamma_epsilon(x)[1]))
 
     # part 2 solution
-    print (oxygen * co2)
-
-
-
-
+    print(oxygen * co2)
