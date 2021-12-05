7898bf6181cca7db40743832aef8a66a868fdadc
Brenda Holloway <brendahol@gmail.com>
2021-12-05 07:39:21 -0500

    Code golf puzzle 5 a little.

--- adventofcode2021/puzzle5.py
+++ adventofcode2021/puzzle5.py
@@ -1,61 +1,21 @@
-# the fifth puzzle
-
 import re
 
-fn = 'puzzle5.dat'
+def delta(x): return 0 if x == 0 else int(x/abs(x))
 
-# make a 10x10 grid
-grid = [[0 for x in range(1000)] for y in range(1000)]
-
-
-def open_file():
-    "open the file and yield the lines"
-    with open(fn) as f:
-        for line in f:
-            yield parse_line(line.strip())
-
-
-def parse_line(line):
-    m = re.match(r'(\d+),(\d+)\s\-\>\s(\d+),(\d+)', line)
-    assert m
-    return (int(m.group(1)), int(m.group(2))), (int(m.group(3)), int(m.group(4)))
-
-
-def sanity_check():
-    print(grid)
-    for start, dest in open_file():
-        print(start, dest)
-
-
-def draw_line(start, dest, exclude_diag=False):
-    # draw the line
-    if exclude_diag and start[0] != dest[0] and start[1] != dest[1]:
-        return
-
-    dx = dest[0] - start[0]
-    if dx != 0:
-        dx = int(dx/abs(dx))
-    dy = dest[1] - start[1]
-    if dy != 0:
-        dy = int(dy/abs(dy))
-
-    x = start[0]
-    y = start[1]
+def draw_line(start, dest, grid):
+    dx, dy = (delta(dest[0] - start[0]), delta(dest[1] - start[1]))
+    x, y = start
 
     while True:
         grid[y][x] += 1
-        if x == dest[0] and y == dest[1]:
-            break
+        if (x,y) == dest: break
         x += dx
         y += dy
 
-
-def score_grid():
-    "return number of elements in grid that are > 1"
-    return sum(sum(1 for x in row if x > 1) for row in grid)
-
-
-if __name__ == "__main__":
-    for start, dest in open_file():
-        draw_line(start, dest, False)
-    print(score_grid())
+with open('puzzle5.dat') as f:
+    grid = [[0 for x in range(1000)] for y in range(1000)]
+    for line in f:
+        m = re.match(r'(\d+),(\d+)\s\-\>\s(\d+),(\d+)', line.strip())
+        start, dest = (int(m.group(1)), int(m.group(2))), (int(m.group(3)), int(m.group(4)))
+        draw_line(start, dest, grid)
+    print(sum(sum(1 for x in row if x > 1) for row in grid))
