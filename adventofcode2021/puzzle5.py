cd60264122f3e96200b3e1614030e141dcdd7bd3
Brenda Holloway <brendahol@gmail.com>
2021-12-05 22:37:10 -0500

    Make it sleeker

--- adventofcode2021/puzzle5.py
+++ adventofcode2021/puzzle5.py
@@ -2,9 +2,8 @@ import re
 
 def delta(x): return 0 if x == 0 else int(x/abs(x))
 
-def draw_line(start, dest, grid):
-    dx, dy = (delta(dest[0] - start[0]), delta(dest[1] - start[1]))
-    x, y = start
+def draw_line(x, y, dest, grid):
+    dx, dy = (delta(dest[0] - x), delta(dest[1] - y))
 
     while True:
         grid[y][x] += 1
@@ -16,6 +15,5 @@ with open('puzzle5.dat') as f:
     grid = [[0 for x in range(1000)] for y in range(1000)]
     for line in f:
         m = re.match(r'(\d+),(\d+)\s\-\>\s(\d+),(\d+)', line.strip())
-        start, dest = (int(m.group(1)), int(m.group(2))), (int(m.group(3)), int(m.group(4)))
-        draw_line(start, dest, grid)
+        draw_line(int(m.group(1)), int(m.group(2)), (int(m.group(3)), int(m.group(4))), grid)
     print(sum(sum(1 for x in row if x > 1) for row in grid))
