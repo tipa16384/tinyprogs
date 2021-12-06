dd680b36ca0a12ea325860ab316349cf575845cd
Brenda Holloway <brendahol@gmail.com>
2021-12-06 07:53:31 -0500

    Golfed puzzle6 a little.

--- adventofcode2021/puzzle6.py
+++ adventofcode2021/puzzle6.py
@@ -7,13 +7,17 @@ def read_ages():
         return [int(x) for x in f.read().strip().split(',')]
     raise Exception('could not read file')
 
-buckets = [0 for i in range(0,9)]
-for fish in read_ages():
-    buckets[int(fish)] += 1
-for i in range(256):
-    cycle = buckets[0]
-    for i in range(1,9):
-        buckets[i-1] = buckets[i]
-    buckets[8] = cycle
-    buckets[6] += cycle
-print (sum(buckets))
+def solve(days):
+    fishes = read_ages()
+    buckets = [fishes.count(i) for i in range(9)]
+    for i in range(days):
+        cycle = buckets[0]
+        for i in range(1,9): buckets[i-1] = buckets[i]
+        buckets[8] = cycle
+        buckets[6] += cycle
+    print (sum(buckets))
+
+if __name__ == '__main__':
+    solve(80)
+    solve(256)
+
