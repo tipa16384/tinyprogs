0b707c6cc009d452749b99f0fe665b8258a63664
Brenda Holloway <brendahol@gmail.com>
2021-12-07 12:59:24 -0500

    I'm closing the IDE now.

--- adventofcode2021/puzzle7.py
+++ adventofcode2021/puzzle7.py
@@ -1,11 +1,8 @@
 import statistics
 
-def crab_fuel(pos, dest, weight):
-    return weight(abs(pos-dest))
-
 def print_min(data, statfn, weight):
     dest = int(statfn(data))
-    print(min(sum(crab_fuel(x, dest+y, weight) for x in data) for y in range(2)))
+    print(min(sum(weight(abs(x-dest-y)) for x in data) for y in range(2)))
 
 with open('puzzle7.dat') as f:
     data = list(map(int, f.read().strip().split(',')))
