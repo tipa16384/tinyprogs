092b23d5460699834f520aa97b4606c5d10d2bd8
Brenda Holloway <brendahol@gmail.com>
2021-12-07 12:53:36 -0500

    Gotta stop fiddling with this.

--- adventofcode2021/puzzle7.py
+++ adventofcode2021/puzzle7.py
@@ -3,11 +3,12 @@ import statistics
 def crab_fuel(pos, dest, weight):
     return weight(abs(pos-dest))
 
-def print_min(data, dest, weight):
+def print_min(data, statfn, weight):
+    dest = int(statfn(data))
     print(min(sum(crab_fuel(x, dest+y, weight) for x in data) for y in range(2)))
 
 with open('puzzle7.dat') as f:
     data = list(map(int, f.read().strip().split(',')))
 
-print_min(data, int(statistics.median(data)), lambda x: x)
-print_min(data, int(statistics.mean(data)), lambda x: x * (x + 1) // 2)
+print_min(data, statistics.median, lambda x: x)
+print_min(data, statistics.mean, lambda x: x * (x + 1) // 2)
