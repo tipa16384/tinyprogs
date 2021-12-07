c98de732ec456feddab2ca6b57de022220085c65
Brenda Holloway <brendahol@gmail.com>
2021-12-07 12:44:33 -0500

    more optimizations for puzzle 7

--- adventofcode2021/puzzle7.py
+++ adventofcode2021/puzzle7.py
@@ -1,19 +1,13 @@
 import statistics
 
-def crab_fuel(pos, dest):
-    dist = abs(pos-dest)
-    return (dist * (dist + 1)) // 2
+def crab_fuel(pos, dest, weight):
+    return weight(abs(pos-dest))
+
+def print_min(data, dest, weight):
+    print(min(sum(crab_fuel(x, dest+y, weight) for x in data) for y in range(2)))
 
 with open('puzzle7.dat') as f:
     data = list(map(int, f.read().strip().split(',')))
 
-median = int(statistics.median(data))
-
-print(sum([abs(x-median) for x in data]))
-
-mean = int(statistics.mean(data))
-
-val1 = sum([crab_fuel(x, mean) for x in data])
-val2 = sum([crab_fuel(x, mean+1) for x in data])
-
-print(min(val1, val2))
+print_min(data, int(statistics.median(data)), lambda x: x)
+print_min(data, int(statistics.mean(data)), lambda x: x * (x + 1) // 2)
