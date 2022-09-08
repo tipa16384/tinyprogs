664218af7edbeaea29788ff0b9c744fd909bcf7a
Brenda Holloway <brendahol@gmail.com>
2022-09-07 22:43:45 -0400

    Make the back end generic, move context to front end and improve conversation threads.

--- openai/oai.py
+++ openai/oai.py
@@ -3,28 +3,10 @@ import json
 import os
 from flask import Flask, request
 
+# start an http server to listen for requests on the /terrachat endpoint
+app = Flask(__name__)
+
 ai_model = 'text-davinci-002'
-start_sequence = "\nTerra:"
-restart_sequence = "\nYou: "
-
-prompt="""Terra is a chatbot who responds as if they were the playable character from Final Fantasy 6. You are
-Celes, a member of the resistance against the Empire. You and Terra are rivals for the love of Locke. Locke is a
-master thief who feels responsible for the death of his lover. Locke isn't sure if he likes you or Terra better,
-but he is leaning toward Terra, but he doesn't want to hurt your feelings. Setzer is a gambler and the captain of the airship
-the Blackjack. He acts as a mentor to the adventurers. Gau is a feral beast child who speaks in the third person using
-very simple language. He is a confidante to you. Edgar Figaro is the prince of Figaro Castle. He is an engineer. His
-twin brother, Sabin, is a monk. After the death of their father, they went their separate ways, but now Sabin has returned.
-Cyan is a samurai with the power of Bushido. He is enemies with the ninja Shadow. Shadow's past is a mystery. His dog
-is named Interceptor, and is fiercely loyal to Shadow. Relm Arrowny is a young artist and secretely Relm's daughter.
-Terra only wants to answer questions about Final Fantasy 6 and is reluctant to talk about anything else.
-Terra is rebelling against the Imperial Empire and desires nothing more than for humans and espers to live in peace and
-harmony:
-
-Terra: Hello, I am Terra! I answer questions about Final Fantasy 6. Is... there anything you want to ask me?
-You: Who are your parents?
-Terra: I am the daughter of a human woman and an esper. Is there anything else you'd like to know?
-You: What dances can Mog learn?
-Terra: Mog can learn the dances Twilight Requiem, Forest Nocture, Desert Lullaby, Earth Blues, Love Serenade, Wind Rhapsody, Water Harmony, and Snowman Rondo. Is there anything else you'd like to know?"""
 
 def initialize_openai():
     # read the api key from the environment variable OPENAI_API_KEY
@@ -38,7 +20,6 @@ def initialize_openai():
             print('Please set the OPENAI_API_KEY environment variable or create a file openai_api_key.txt with your OpenAI API key in it.')
             exit(1)
     openai.api_key = api_key
-
 
 def get_models():
     models = openai.Model.list()
@@ -61,34 +42,13 @@ def gpt3(xprompt, engine='text-davinci-002', response_length=64,
         stop=stop_seq,
     )
     answer = response.choices[0]['text']
-    new_prompt = prompt + start_text + answer + restart_text
+    new_prompt = xprompt + start_text + answer + restart_text
     return answer, new_prompt
-
-def chat():
-    print ("Terra: Hello, I am Terra! I answer questions about Final Fantasy 6. Is... there anything you want to ask me?")
-    xprompt = prompt
-    while True:
-        #xprompt = prompt + input(restart_sequence)
-        answer, xprompt = gpt3(xprompt + input(restart_sequence),
-                              temperature=0.75,
-                              frequency_penalty=0,
-                              presence_penalty=0.6,
-                              response_length=150,
-                              top_p=1,
-                              start_text=start_sequence,
-                              restart_text=restart_sequence,
-                              stop_seq=[restart_sequence, '\n'])
-        print('Terra: ' + answer)
-
-# start an http server to listen for requests on the /terrachat endpoint
-app = Flask(__name__)
-
-initialize_openai()
 
 # add a flask endpoint that returns the file index.html when the user visits the root url
 @app.route('/')
 def index():
-    print ('hit the root')
+#    print ('hit the root')
     return app.send_static_file('index.html')
 
 # add a flask endoint that responsed to GET requests to /hello with "Hello World!"
@@ -105,8 +65,12 @@ def fav():
 def terrachat():
     data = request.get_json()
     print (data)
-    xprompt = prompt + str(data['prompt'])
-    answer, xprompt = gpt3(xprompt,
+    xprompt = str(data['prompt'])
+    start_sequence = str(data['start_sequence'])
+    restart_sequence = str(data['restart_sequence'])
+
+#    print ("Input to gpt3: " + xprompt)
+    answer, _ = gpt3(xprompt,
                             temperature=0.75,
                             frequency_penalty=0,
                             presence_penalty=0.6,
@@ -114,12 +78,13 @@ def terrachat():
                             top_p=1,
                             start_text=start_sequence,
                             restart_text=restart_sequence,
-                            stop_seq=[restart_sequence, '\n'])
+                            stop_seq=[restart_sequence, '\n']);
+    xprompt += start_sequence + answer;
+#    print ('return answer: ' + answer)
+
     return json.dumps({'prompt': xprompt, 'answer': answer})
 
-app.run()
-
 # if main module
-# if __name__ == '__main__':
-#     initialize_openai()
-#     chat()
+if __name__ == '__main__':
+    initialize_openai()
+    app.run()
